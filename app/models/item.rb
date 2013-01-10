352f6d43e835967d642c243814e85052a66ad682
alex <alex@alex-pc.(none)>
2013-01-10 21:03:41 +0400

    added item model and assosiation with user

---
+++ app/models/item.rb
@@ -0,0 +1,5 @@
+class Item < ActiveRecord::Base
+  attr_accessible :description, :title
+  
+  belongs_to :user
+end
---
+++ db/migrate/20130110165602_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string :title
+      t.text :description
+      t.reference :user
+
+      t.timestamps
+    end
+  end
+end
