ea7d1094dd66a773cbf0992d94533bee89931a76
alex <alex@alex-pc.(none)>
2013-01-11 11:05:49 +0400

    typo fixed in Item model with assosiations

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ActiveRecord::Base
   attr_accessible :description, :title
   
   belongs_to :user
-  has_many: :messages
-  has_many: :photos
+  has_many :messages
+  has_many :photos
   has_and_belongs_to_many :tags
 end
